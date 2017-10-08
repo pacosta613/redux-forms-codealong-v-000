5c8ff570cddacce3112b2294d61e634b5258e9f2
Pedro Acosta <pedro.acosta613@gmail.com>
2017-10-08 01:33:03 +0000

    automatically backed up by learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,16 +1,11 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  switch (action.type){
+    case "ADD_TODO":
+    return {todos: state.todos.concat(action.todo.text)} ;
 
-  switch (action.type) {
-    case 'ADD_TODO':
-
-      return { todos: state.todos.concat(action.todo.text) };
-
-    default:
-      return state;
-
+  default:
+    return state;
   }
-
-
 }
